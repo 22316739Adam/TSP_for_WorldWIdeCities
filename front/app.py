92164536a619010756b0820f617b62f70b90633b
22316739Adam <22316739@student.ciu.edu.tr>
2026-07-14 19:05:23 +0100

    Update app.py

--- front/app.py
+++ front/app.py
@@ -37,23 +37,25 @@ for i in range(num_cities):
     country = st.text_input(
         f"Country Code for City {i + 1}"
     )
-
     cities_with_cc.append((city, country))
 
 # solve button
 if st.button("Solve TSP"):
-    st.write("Cities entered:")
+    st.write("Cities entered:", cities_with_cc)
 
     c_idxs = [i for i in range(num_cities)]
 
-    for city in cities_with_cc:
-        st.write(city)
-
     cities_data = get_cities_data(cities_with_cc)
-    distance_matrix = build_mtx(cities_data)
-    best_route, best_distance = tsp_for(c_idxs, distance_matrix)
 
-    solution = [cities[i] for i in c_idxs]
+    distance_matrix = build_mtx(cities_data)
+
+    best_route, best_distance = tsp_for(c_idxs, distance_matrix)
+    st.write(best_route, best_distance)
+
+    solution = [cities[i] for i in best_route]
 
     st.write(f"Best route found: {solution} with total distance of {best_distance}")
+
+
+    
     
