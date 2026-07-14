878fd8d2e941396a3daa3109e50ecb097fcf4393
Adam ElBoudali Zahid <adamzahid048@gmail.com>
2026-07-14 15:02:45 +0100

    Fixing bugs and updating the algo

---
+++ bEnd/comp_distance.py
@@ -0,0 +1,39 @@
+#Haversine formula would be used in order to compute accurate distance between two cities on Earth
+
+from math import radians, cos, sin, sqrt, atan2
+
+EARTH_RADIUS = 6371
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    d_lat = lat2 - lat1
+    d_lon = lon2 - lon1
+
+    a = (sin(d_lat/2) ** 2 + cos(lat1) * cos(lat2) * sin(d_lon/2) ** 2)
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+
+    distance = EARTH_RADIUS * c
+    return distance
+
+
+def build_mtx(cities):
+    n = len(cities)
+
+    mtx = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if i==j: continue
+
+            city1 = cities[i]
+            city2 = cities[j]
+
+            #After computing the distance between each two cities, distance matrix could be created
+            distance = haversine_distance(city1["latitude"], city1["longitude"], city2["latitude"], city2["longitude"])
+            mtx[i][j] = round(distance, 5) #At 5 points of precision
+
+    return mtx 
---
+++ front/app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from coo_db.retrieval_fromdb import get_cities_data
+from bEnd.comp_distance import build_mtx
+from bEnd.tsp import tsp_for
+
+st.title("Travelling Salesman algorithm for shortest distance between cities")
+
+st.write("Enter the number of cities wanted: ")
+
+# Computing number of cities
+num_cities = st.number_input(
+    "Number of cities:",
+    min_value=2,
+    max_value=100, #theoretical limit
+    step=1
+)
+
+cities = []
+cities_with_cc = []
+
+# dynamic city inputs
+for i in range(num_cities):
+    # textfield for city_name
+    city = st.text_input(
+        f"City {i + 1}"
+    )
+
+    cities.append(city)
+
+    # textfield for country code
+    country = st.text_input(
+        f"Country Code for City {i + 1}"
+    )
+    cities_with_cc.append((city, country))
+
+# solve button
+if st.button("Solve TSP"):
+    st.write("Cities entered:", cities_with_cc)
+
+    c_idxs = [i for i in range(num_cities)]
+
+    cities_data = get_cities_data(cities_with_cc)
+
+    distance_matrix = build_mtx(cities_data)
+
+    best_route, best_distance = tsp_for(c_idxs, distance_matrix)
+    st.write(best_route, best_distance)
+
+    # solution = [cities[i] for i in best_route]
+
+    # st.write(f"Best route found: {solution} with total distance of {best_distance}")
+
+
+    
